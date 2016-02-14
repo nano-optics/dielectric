eb1574a65717553d7bb64bf3d7e4a78881449f92
baptiste <baptiste.auguie@gmail.com>
2016-02-15 09:15:28 +1300

    docs

---
+++ R/datasets.r
@@ -0,0 +1,14 @@
+#' Silver in the visible, from Palik
+#'
+#' Dielectric dataset corresponding to the values of Palik.
+#'
+#' @format A dielectric reference class with 4 fields
+#' \describe{
+#'   \item{wavelength}{price, in US dollars}
+#'   \item{epsilon}{weight of the diamond, in carats}
+#'   \item{span}{weight of the diamond, in carats}
+#'   \item{epsilon}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"AgPalik"
